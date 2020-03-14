ab4140cf6288b480ef423e00cad1e8366ba9e2a4
Arith Alexis Villalba Bravo <arithalexis@gmail.com>
2020-03-14 17:48:28 -0500

    dto added

--- src/modules/income/income.service.ts
+++ src/modules/income/income.service.ts
@@ -3,11 +3,34 @@ import { Repository } from 'typeorm';
 
 import { HISTORICAL_INCOME_REPOSITORY } from 'src/config';
 import { HistoricalIncome } from '../entities/historical_income.entity';
+import { IncomeDto } from './dto/income.dto';
 
 @Injectable()
 export class IncomeService {
     constructor(
         @Inject(HISTORICAL_INCOME_REPOSITORY)
         private readonly historicalIncome: Repository<HistoricalIncome>
-    ) {}
+    ) { }
+
+    async create(incomeDto: IncomeDto) {
+        try {
+            const income = await this.historicalIncome.save(incomeDto);
+
+            return income;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async update(id: number, incomeDto: IncomeDto) {
+        try {
+            let income = await this.historicalIncome.findOne(id);
+
+            if (income) {
+                await this.historicalIncome.save(income, { data: incomeDto });
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 }
